497605035f81c6549c153b5ab1009f5f05fc5208
romemar <rober.mediero@gmail.com>
2021-04-20 18:59:58 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchGvsig from './components/SearchGvsig';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchGvsig />
+    </div>
+  );
+}
+
+export default App;

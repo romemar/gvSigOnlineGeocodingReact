31de58c74b4d0e84f33e62c70ce8d15f0b1c7b77
romemar <rober.mediero@gmail.com>
2021-05-11 14:11:47 +0200

    mejora en búsqueda y clasificación de proveedores

--- src/Layers/TileLayer.js
+++ src/Layers/TileLayer.js
@@ -1,99 +1,169 @@
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MapContext from "../Map/MapContext";
 import OLTileLayer from "ol/layer/Tile";
-import {fromLonLat, toLonLat} from 'ol/proj';
+import { fromLonLat, toLonLat } from "ol/proj";
+import Overlay from "ol/Overlay.js";
+//import Popup from 'ol/overlay'
 
-const reverseGeocode = (evt, url) => {
+const TileLayer = ({ source, zIndex = 0, geoOn, url }) => {
 
-  var coord = toLonLat(evt.coordinate);
-  var type = 'new_cartociudad'
+  const { map } = useContext(MapContext);
 
-  let formBody = [];
+  const allowRevGeo = geoOn;
+  const gvSigUrl = url;
+  const [popupPosition, setPopupPosition] = useState([0, 0]);
+  const [popupText, setPopupText] = useState("here");
+  const popupElement = React.createElement(
+    "div",
+    { className: "ol-popup" },
+    popupText
+  );
 
-  var encodedKeyCoord = encodeURIComponent(coord);
-  formBody.push("coord=" + encodedKeyCoord);
+  const reverseGeocode = (evt, url) => {
+    var coord = toLonLat(evt.coordinate);
+    console.log(coord);
+    var type = "new_cartociudad";
 
-  var encodedKeyType = encodeURIComponent(type);
-  formBody.push("type=" + encodedKeyType);
+    let formBody = [];
 
-  formBody = formBody.join("&");
-   //coord=-2.774274873462422%2C41.56326798105209&type=new_cartociudad
-  console.log('FORM_BODY==> '+formBody);
+    var encodedKeyCoord = encodeURIComponent(coord);
+    formBody.push("coord=" + encodedKeyCoord);
 
-  return fetch(url + "/geocoding/get_location_address/", {
-    method: "POST",
-    headers: {
-      //"Content-Type": "application/json"
-      "Content-Type": "application/x-www-form-urlencoded"
-      // "X-CSRFToken": csrftoken
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: formBody // body data type must match "Content-Type" header
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      console.log("get_location_address: " + JSON.stringify(json));
+    var encodedKeyType = encodeURIComponent(type);
+    formBody.push("type=" + encodedKeyType);
 
-      //setCenter(fromLonLat([json.lng,json.lat]))
-      //setZoom(14)
-      let coordLocation = [json.lng, json.lat]
-      console.log(coordLocation)
+    formBody = formBody.join("&");
+    //coord=-2.774274873462422%2C41.56326798105209&type=new_cartociudad
+    console.log("FORM_BODY==> " + formBody);
 
-      alert('Dirección: '+JSON.stringify(json.address)+
-      '\n'+ JSON.stringify(json.muni)+', '+JSON.stringify(json.province)+
-      '\n'+JSON.stringify(json.postalCode)+', '+JSON.stringify(json.comunidadAutonoma)+', Coordenadas: '+coordLocation)
-
-     /*
-      //map.overlays.setPosition(evt.coordinate)
-        //setPopupCoord(evt.coordinate);
-        //alert(toLonLat(evt.coordinate))
-        //console.log(toLonLat(evt.coordinate))
-
-        get_location_address: {"id":"450540332179","province":"Toledo","comunidadAutonoma":"Castilla-La Mancha","muni":"Corral de Almaguer",
-        "type":null,"address":"AP-36","postalCode":"45880","poblacion":null,"geom":"POINT (-3.1208884461448116 39.68808214367244)","tip_via":null,
-        "lat":39.68808214367244,"lng":-3.1208884461448116,"portalNumber":45,"stateMsg":"Resultado exacto de la bÃºsqueda","state":1,"priority":0,
-        "countryCode":"011","refCatastral":null,"source":"new_cartociudad","srs":"EPSG:4258"}
-    */
+    return fetch(url + "/geocoding/get_location_address/", {
+      method: "POST",
+      headers: {
+        //"Content-Type": "application/json"
+        "Content-Type": "application/x-www-form-urlencoded",
+        // "X-CSRFToken": csrftoken
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: formBody, // body data type must match "Content-Type" header
     })
-    .catch((err) => console.error(err.message));
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("get_location_address: " + JSON.stringify(json));
 
-  }
+        //setCenter(fromLonLat([json.lng,json.lat]))
+        //setZoom(14)
+        let coordLocation = fromLonLat([json.lng, json.lat]);
+        setPopupPosition(coordLocation);
 
-const TileLayer = ({ source, zIndex = 0, geoOn, url, setCenter}) => {
-  const { map } = useContext(MapContext); 
+        console.log(coordLocation);
+        var mensaje =
+          "Dirección: " +
+          JSON.stringify(json.tip_via) +
+          JSON.stringify(json.address) +
+          ", " +
+          JSON.stringify(json.portalNumber) +
+          "\n" +
+          JSON.stringify(json.muni) +
+          ", " +
+          JSON.stringify(json.province) +
+          "\n" +
+          JSON.stringify(json.postalCode) +
+          ", " +
+          JSON.stringify(json.comunidadAutonoma) +
+          ", Coordenadas: " +
+          coordLocation;
+        alert(mensaje);
+        setPopupText(mensaje);
+
+        /*
+        //map.overlays.setPosition(evt.coordinate)
+          //setPopupCoord(evt.coordinate);
+          //alert(toLonLat(evt.coordinate))
+          //console.log(toLonLat(evt.coordinate))
   
-  const allowRevGeo = geoOn
-  const gvSigUrl = url
+          get_location_address: {"id":"450540332179","province":"Toledo","comunidadAutonoma":"Castilla-La Mancha","muni":"Corral de Almaguer",
+          "type":null,"address":"AP-36","postalCode":"45880","poblacion":null,"geom":"POINT (-3.1208884461448116 39.68808214367244)","tip_via":null,
+          "lat":39.68808214367244,"lng":-3.1208884461448116,"portalNumber":45,"stateMsg":"Resultado exacto de la bÃºsqueda","state":1,"priority":0,
+          "countryCode":"011","refCatastral":null,"source":"new_cartociudad","srs":"EPSG:4258"}
+      */
+      })
+      .catch((err) => console.error(err.message));
+  };
+
   
   useEffect(() => {
     if (!map) return;
-    
+
     let tileLayer = new OLTileLayer({
       source,
       zIndex,
-      allowRevGeo, 
-      setCenter
+      allowRevGeo,
     });
 
-    console.log('Activado? '+allowRevGeo)
-    
-    //if(allowRevGeo){
-      map.on("singleclick", evt => {
-        reverseGeocode(evt, gvSigUrl)
-
-      });
-   //}
     map.addLayer(tileLayer);
-
-    
     tileLayer.setZIndex(zIndex);
+    /*
+    const overlay = new Overlay({
+      position: popupPosition,
+      element: popupElement,
+      positioning: 'center-center',
+      stopEvent: false
+    });
+    map.addOverlay(overlay);
+*/
+
     return () => {
       if (map) {
         map.removeLayer(tileLayer);
       }
     };
   }, [map]);
+
+  useEffect(() => {
+    if (!map) return;
+    console.log("Activado? " + allowRevGeo);
+
+    if (allowRevGeo) {
+      map.on("singleclick", (evt) => {
+        reverseGeocode(evt, gvSigUrl);
+      });
+    } else {
+      map.un("singleclick", (evt) => {
+        reverseGeocode(evt, gvSigUrl);
+      });
+    }
+
+    /*
+    if(!allowRevGeo) { 
+    map.un("singleclick", (evt) => {
+        reverseGeocode(evt, gvSigUrl);
+      })
+  }
+
+  if(!allowRevGeo) { 
+      map.removeEventListener('singleclick');
+    }
+  
+  */
+    /*
+    const overlay = new Overlay({
+      position: popupPosition,
+      element: popupElement,
+      positioning: 'center-center',
+      stopEvent: false
+    });
+    map.addOverlay(overlay);
+*/
+
+return () => {
+  if (map) {
+    map.un("singleclick", (evt) => {
+      reverseGeocode(evt, gvSigUrl);
+    });
+  }
+};
+  }, [allowRevGeo]);
   return null;
 };
 export default TileLayer;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,50 +1,47 @@
-import React, {useState, useContext} from 'react'
-import "../../src/App.css"
+import React, { useState, useContext } from "react";
+import "../../src/App.css";
 //Modulo Ant Design ---- https://ant.design/
-import {ClearOutlined} from '@ant-design/icons'
-import { Input, AutoComplete } from 'antd';
-import 'antd/dist/antd.css';
-import { Button, Tooltip } from 'antd';
-//Usamos OpenLayers para incluir mapa  
+import { ClearOutlined } from "@ant-design/icons";
+import { Input, AutoComplete } from "antd";
+import "antd/dist/antd.css";
+import { Button, Tooltip } from "antd";
+//Usamos OpenLayers para incluir mapa
 import Map from "../Map";
 import { osm, vector } from "../Source";
-import { fromLonLat, get, toLonLat } from 'ol/proj';
-import { Layers, TileLayer, VectorLayer, RevGeocodingLayer} from "../Layers";
-import { Circle as CircleStyle, Fill, Stroke, Style, Icon} from 'ol/style';
-import GeoJSON from 'ol/format/GeoJSON';
-import Overlay from "ol/Overlay";
-//instalamos paquete @fortawesome/react-fontawesome' para usar iconos de 
+import { fromLonLat, get, toLonLat } from "ol/proj";
+import { Layers, TileLayer, VectorLayer } from "../Layers";
+import { Circle as CircleStyle, Fill, Stroke, Style, Icon } from "ol/style";
+import GeoJSON from "ol/format/GeoJSON";
+
+//instalamos paquete @fortawesome/react-fontawesome' para usar iconos de
 //https://fontawesome.com/icons?d=gallery&p=2
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMap, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMap, faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+
+
 
 const SearchBox = () => {
 
-  const [options, setOptions] = useState(); 
+  const [options, setOptions] = useState();
   const [gvSigUrl, setGvSigUrl] = useState("https://localhost/gvsigonline");
   const [center, setCenter] = useState([-3.70256, 40.4165]);
   const [zoom, setZoom] = useState(6);
-  const [searchText, setSearchText]=useState('');
-  const [resultsSearch, setResultsSearch]=useState();
+  const [searchText, setSearchText] = useState("");
+  const [resultsSearch, setResultsSearch] = useState();
   const [showLayer, setShowLayer] = useState(false);
-  const [geojsonObject, setGeojsonObject]= useState();
-  const [reverseGeocodingOn, setReverseGeocodingOn]= useState(false);
+  const [geojsonObject, setGeojsonObject] = useState();
+  const [reverseGeocodingOn, setReverseGeocodingOn] = useState(false);
 
-
-  const renderTitle = (title) => (
-    <span>
-      {title}
-    </span>
-  );
+  const renderTitle = (title) => <span>{title}</span>;
 
   const renderItem = (s) => ({
-    key:s.id,
+    key: s.id,
     value: s.address,
     label: (
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
+          display: "flex",
+          justifyContent: "space-between",
         }}
       >
         {s.address}
@@ -52,79 +49,63 @@ const SearchBox = () => {
     ),
   });
 
-  const lookup = async (userInput) =>{
 
+
+  const lookup = async (userInput) => {
     const candidatos = [];
-  
+
     let response = await fetch(
       gvSigUrl + "/geocoding/search_candidates/?limit=10&q=" + userInput
     );
-  
+
     let json = await response.json();
     console.log(json);
     let results = json.suggestions;
-    setResultsSearch(results)
-    console.log(resultsSearch)
-
-    let optionNominatim = []
-    let optionCartociudad = []
-    let optionGoogle = []
+    setResultsSearch(results);
 
     if (results.length > 0) {
+      //recogemos los proveedores de búsqueda que están activos
+      let proveedores = [];
       results.map((suggest) => {
-        
-      if (suggest.category==="Nominatim"){
-        
-        optionNominatim.push(renderItem(suggest))
-      }
-      if (suggest.category==="Cartociudad"){
-        
-        optionCartociudad.push(renderItem(suggest))
-      }
-      if (suggest.category==="Google"){
-        
-        optionGoogle.push(renderItem(suggest))
-      }
-      
-      }
-      )
+        proveedores.push(suggest.category);
+      });
 
-      candidatos.push({
-        label: renderTitle('Nominatim'),
-        options: optionNominatim,
-      })
-      candidatos.push({
-        label: renderTitle('Cartociudad'),
-        options: optionCartociudad,
-      })
-      candidatos.push({
-        label: renderTitle('Google'),
-        options: optionGoogle,
-      })
+      let uniqueProveedores = [...new Set(proveedores)];
+      console.log("Proveedores: " + uniqueProveedores);
 
+      //recogemos para cada proveedor sus candidatos de búsqueda
+      uniqueProveedores.map((prov) => {
+        const arrayOptions = [];
+        results.map((suggest) => {
+          if (suggest.category === prov) {
+            arrayOptions.push(renderItem(suggest));
+          }
+        });
+        candidatos.push({
+          label: renderTitle(prov),
+          options: arrayOptions,
+        });
+      });
     }
-     // if
     setOptions(candidatos);
-    
-  } 
- 
+  };
 
-// Con Geolookup las opciones de la lista () const getSuggestLabel = (s) => return s.address;};
-// EN ESTE CASO NUESTRA LISTA NO SON OBJECTOS, ES UN TITULO(Texto) CON LA DIRECCIÓN DEL OBJETO
+
+
+  // Con Geolookup las opciones de la lista () const getSuggestLabel = (s) => return s.address;};
+  // EN ESTE CASO NUESTRA LISTA NO SON OBJECTOS, ES UN TITULO(Texto) CON LA DIRECCIÓN DEL OBJETO
 
   const geocode = (value) => {
-
-    if (showLayer){
-      setShowLayer(false)
+    if (showLayer) {
+      setShowLayer(false);
     }
     //recorremos el array de resultados en busca de la localización seleccionada
     //HABRÁ QUE BUSCAR EL OBJETO DENTRO DE RESULTS (USAREMOS useState)
-    let suggest=[]
+    let suggest = [];
 
-    for (var i=0; i < resultsSearch.length; i++){
-
-      if (resultsSearch[i].address === value){
-        suggest.push(resultsSearch[i])
+    for (var i = 0; i < resultsSearch.length; i++) {
+      if (resultsSearch[i].address === value) {
+        suggest.push(resultsSearch[i]);
         console.log(resultsSearch[i]);
       }
     }
@@ -137,7 +118,7 @@ const SearchBox = () => {
 
     // FJP: Esta parte es probable que tenga que cambiarse en gvSIG Online. Jose envía de una forma muy rara
     // la dirección:
-    
+
     let formBody = [];
     for (let property in suggest[0]) {
       var encodedKey = encodeURIComponent("address[" + property + "]");
@@ -153,170 +134,203 @@ const SearchBox = () => {
       method: "POST",
       headers: {
         //"Content-Type": "application/json"
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
         // "X-CSRFToken": csrftoken
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: formBody // body data type must match "Content-Type" header
+      body: formBody, // body data type must match "Content-Type" header
     })
       .then((response) => response.json())
       .then((json) => {
         console.log("find_candidate: " + JSON.stringify(json));
         let encontrados = json.address;
-        var coordenadas= "lat: "+encontrados.lat+" - lng: "+encontrados.lng;
-        setCenter([encontrados.lng,encontrados.lat])
-        setZoom(14)
+        var coordenadas =
+          "lat: " + encontrados.lat + " - lng: " + encontrados.lng;
+        setCenter([encontrados.lng, encontrados.lat]);
+        setZoom(14);
         setGeojsonObject({
-          "type": "FeatureCollection",
-          "features": [
+          type: "FeatureCollection",
+          features: [
             {
-              "type": "Feature",
-              "properties": {},
-              "geometry": {
-                "type": "Point",
-                "coordinates": [
-                  encontrados.lng,
-                  encontrados.lat]
-              }
-            }
-          ]
-      })
-        setShowLayer(true)
-       
-        console.log("Coordenadas ->"+coordenadas);
-        //alert("Toca hacer zoom en" + coordenadas);
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Point",
+                coordinates: [encontrados.lng, encontrados.lat],
+              },
+            },
+          ],
+        });
+        setShowLayer(true);
 
+        console.log("Coordenadas ->" + coordenadas);
       })
       .catch((err) => console.error(err.message));
     // return new Promise((resolve, reject) => {});
-  }
+  };
+
+
 
   //función borrar datos de búsqueda y volver a zoom inicial
   const onCLickDelete = () => {
-    setCenter([-3.70256, 40.4165,]);
+    setCenter([-3.70256, 40.4165]);
     setZoom(6);
-    setSearchText('');
+    setSearchText("");
     setOptions([]);
     setResultsSearch();
-    setShowLayer(false)
-  } 
+    setShowLayer(false);
+  };
+
+
 
   return (
     <div className="App">
       <div>
-      <AutoComplete
-        value={searchText}
-        dropdownClassName="certain-category-search-dropdown"
-        dropdownMatchSelectWidth={500}
-        style={{
-          marginTop:20,
-          width: 350,
-          textAlign:'left',
-        }}
-        options={options}
-        onChange={(text)=>{
-                  lookup(text)
-                  setSearchText(text)}}
-        onSelect={(value)=>{geocode(value)}}
-      >
-
-       {resultsSearch? 
-          <Input
-              size="large" 
+        <AutoComplete
+          value={searchText}
+          dropdownClassName="certain-category-search-dropdown"
+          dropdownMatchSelectWidth={500}
+          style={{
+            marginTop: 20,
+            width: 350,
+            textAlign: "left",
+          }}
+          options={options}
+          onChange={(text) => {
+            lookup(text);
+            setSearchText(text);
+          }}
+          onSelect={(value) => {
+            geocode(value);
+          }}
+        >
+          {resultsSearch ? (
+            <Input
+              size="large"
               placeholder="Buscar..."
               suffix={
-              <Tooltip title="Borrar">
-                <Button size='small'  shape="circle" icon={<ClearOutlined/>} onClick={onCLickDelete}/>
-              </Tooltip>
-              }  
+                <Tooltip title="Borrar">
+                  <Button
+                    size="small"
+                    shape="circle"
+                    icon={<ClearOutlined />}
+                    onClick={onCLickDelete}
+                  />
+                </Tooltip>
+              }
               addonAfter={
                 <Tooltip title="Geocodificador inverso">
-                  {reverseGeocodingOn ?  
-                    <Button 
-                      style={{marginLeft:5}} 
-                      shape="circle" 
-                      icon={<FontAwesomeIcon icon={faMapMarkedAlt}/>} 
-                      onClick={()=> {setReverseGeocodingOn(false); console.log('Geocodificador inverso desactivado')}}/> 
-                      :
-                    <Button 
-                      style={{marginLeft:5}} 
-                      type="primary" 
-                      shape="circle" 
-                      icon={<FontAwesomeIcon icon={faMapMarkedAlt}/>} 
-                      onClick={()=> {setReverseGeocodingOn(true); console.log('Geocodificador inverso activado')}}/> 
-                  }
-                </Tooltip> }
-            /> 
-          :
-          <Input
-              size="large" 
-              placeholder="Buscar..." 
+                  {reverseGeocodingOn ? (
+                    <Button
+                      style={{ marginLeft: 5 }}
+                      shape="circle"
+                      icon={<FontAwesomeIcon icon={faMapMarkedAlt} />}
+                      onClick={() => {
+                        setReverseGeocodingOn(false);
+                        console.log("Geocodificador inverso desactivado");
+                      }}
+                    />
+                  ) : (
+                    <Button
+                      style={{ marginLeft: 5 }}
+                      type="primary"
+                      shape="circle"
+                      icon={<FontAwesomeIcon icon={faMapMarkedAlt} />}
+                      onClick={() => {
+                        setReverseGeocodingOn(true);
+                        console.log("Geocodificador inverso activado");
+                      }}
+                    />
+                  )}
+                </Tooltip>
+              }
+            />
+          ) : (
+            <Input
+              size="large"
+              placeholder="Buscar..."
               addonAfter={
                 <Tooltip title="Geocodificador inverso">
-                  {reverseGeocodingOn ?  
-                    <Button 
-                      style={{marginLeft:5}} 
-                      shape="circle" 
-                      icon={<FontAwesomeIcon icon={faMapMarkedAlt}/>} 
-                      onClick={()=> {setReverseGeocodingOn(false); console.log('Geocodificador inverso desactivado')}}/> 
-                      :
-                    <Button 
-                      style={{marginLeft:5}} 
-                      type="primary" 
-                      shape="circle" 
-                      icon={<FontAwesomeIcon icon={faMapMarkedAlt}/>} 
-                      onClick={()=> {setReverseGeocodingOn(true); console.log('Geocodificador inverso activado');}}/> 
-                  }
-                </Tooltip> }/>
-        } 
-      </AutoComplete>
+                  {reverseGeocodingOn ? (
+                    <Button
+                      style={{ marginLeft: 5 }}
+                      shape="circle"
+                      icon={<FontAwesomeIcon icon={faMapMarkedAlt} />}
+                      onClick={() => {
+                        setReverseGeocodingOn(false);
+                        console.log("Geocodificador inverso desactivado");
+                      }}
+                    />
+                  ) : (
+                    <Button
+                      style={{ marginLeft: 5 }}
+                      type="primary"
+                      shape="circle"
+                      icon={<FontAwesomeIcon icon={faMapMarkedAlt} />}
+                      onClick={() => {
+                        setReverseGeocodingOn(true);
+                        console.log("Geocodificador inverso activado");
+                      }}
+                    />
+                  )}
+                </Tooltip>
+              }
+            />
+          )}
+        </AutoComplete>
       </div>
 
-    <div>
-      <Map 
-        center={fromLonLat(center)} 
-        zoom={zoom}
-      >
-        <Layers>
-              <TileLayer
-                source={osm()}
-                zIndex={0} 
-                geoOn={reverseGeocodingOn}
-                url={gvSigUrl}
-                center={center}
-              />
-            {showLayer && (
-            <VectorLayer
-              source={vector({ features: new GeoJSON().readFeatures(geojsonObject, { featureProjection: get('EPSG:3857') }) })}
-              style={styles.Point}
+      <div>
+        <Map center={fromLonLat(center)} zoom={zoom}>
+          <Layers>
+            <TileLayer
+              source={osm()}
+              zIndex={0}
+              geoOn={reverseGeocodingOn}
+              url={gvSigUrl}
+              center={center}
             />
+            {showLayer && (
+              <VectorLayer
+                source={vector({
+                  features: new GeoJSON().readFeatures(geojsonObject, {
+                    featureProjection: get("EPSG:3857"),
+                  }),
+                })}
+                style={styles.Point}
+              />
             )}
-
-        </Layers>
-      </Map>
-     
+          </Layers>
+          {/* <Overlay id="popups" position={popupPosition} positioning="center-center" element={popupElement} offset={[0,0]}/> */}
+        </Map>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
+
+
 
 let styles = {
-	'Point': new Style({
-		image: new CircleStyle({
-			radius: 6,
-			fill: new Fill({color: 'red'}),
-			stroke: new Stroke({
-				color: [0,0,0], width: 2
-			}),
-		}),
-	}),
-  'icon': new Style({
+  Point: new Style({
+    image: new CircleStyle({
+      radius: 6,
+      fill: new Fill({ color: "red" }),
+      stroke: new Stroke({
+        color: [0, 0, 0],
+        width: 2,
+      }),
+    }),
+  }),
+  icon: new Style({
     image: new Icon({
-      anchor: [1, 6],
-      src: '../data/marker.png',
+      anchor: [0.5, 46],
+      anchorXUnits: "fraction",
+      anchorYUnits: "pixels",
+      opacity: 0.75,
+      src: "../data/marker.100x100.png",
     }),
   }),
 };
 
-export default SearchBox
+export default SearchBox;
